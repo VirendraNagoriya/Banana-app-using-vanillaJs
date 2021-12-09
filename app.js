e3f8d170b3bed77cb88ce1bc3367e915f05c66f4
VirendraNagoriya <virendranagoriya11@gmail.com>
2021-12-09 22:24:14 +0530

    Bnana speak translation app

---
+++ app.js
@@ -0,0 +1,20 @@
+ var btnTranslate = document.querySelector("#button1-translate");
+var txtInput = document.querySelector("#txt-input");
+var resultDiv = document.querySelector("#result");
+
+ 
+//  resultDiv.innerText = "hey"
+
+// console.log(resultDiv)
+
+function clickHandler(){
+ // console.log("hurryy!!");
+
+resultDiv.innerText ="nanaaa"+txtInput.value +"nana";
+} ;
+
+ btnTranslate.addEventListener("click",  clickHandler)
+
+
+
+
